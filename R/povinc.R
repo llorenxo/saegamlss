8d906bfdd470aca6abf0383472e5ed68afddfdff
llorenxo <lorenzo.mori7@unibo.it>
2023-06-22 13:01:52 +0200

    Spelling correction

--- R/povinc.R
+++ R/povinc.R
@@ -1,21 +1,19 @@
-
-
 #' Head Count Ratio
 #' @description Compute the Head Count Ration
-#' @param y  A vector with numerical valeus. Usually income or consumption data.
+#' @param y  A vector with numerical values. Usually income or consumption data.
 #' @param z The poverty line. Default is 0.6*median(y)
 #' @return The Head Count Ratio
 #' @export
 #'
-#' @examples y=runif(100, 0, 1000)
-#' @examples povinc(y)
+#' @examples y <- runif(100, 0, 1000)
+#' povinc(y)
 #' @author Lorenzo Mori and Maria Rosaria Ferrante
 
-povinc <- function(y, z=NULL) {
-
-  if (is.null(z)) {z=0.6*median(y)}
+povinc <- function(y, z = NULL) {
+  if (is.null(z)) {
+    z <- 0.6 * median(y)
+  }
 
   result <- mean(y < z)
-  return (result)
-
+  return(result)
 }
